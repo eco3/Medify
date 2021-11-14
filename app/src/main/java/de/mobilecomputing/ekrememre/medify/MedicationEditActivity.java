973c8252e532abda7a4ddd598c843e7f59f01625
Ekrem <ekrem.emre@posteo.de>
2021-11-14 18:28:36 +0100

    some layouting n stuff

--- app/src/main/java/de/mobilecomputing/ekrememre/medify/MedicationEditActivity.java
+++ app/src/main/java/de/mobilecomputing/ekrememre/medify/MedicationEditActivity.java
@@ -9,6 +9,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -18,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.mobilecomputing.ekrememre.medify.entities.AlertTimestamp;
-import de.mobilecomputing.ekrememre.medify.entities.Medication;
 import de.mobilecomputing.ekrememre.medify.recyclerviews.AlertsViewAdapter;
 
 public class MedicationEditActivity extends AppCompatActivity implements AddAlertDialogFragment.AddAlertDialogListener {
@@ -36,7 +38,7 @@ public class MedicationEditActivity extends AppCompatActivity implements AddAler
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medication_edit);
+        setContentView(R.layout.activity_medication);
 
         Intent intent = getIntent();
         int requestCode = intent.getIntExtra(MainActivity.REQUEST_CODE_EXTRA, -1);
@@ -48,13 +50,6 @@ public class MedicationEditActivity extends AppCompatActivity implements AddAler
         mnameEditText = (EditText) findViewById(R.id.name_editText);
         mdescriptionEditText = (EditText) findViewById(R.id.description_editText);
 
-//        if (requestCode == MainActivity.ADD_MEDICATION_REQUEST_CODE) {
-//            int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-//            int currentMinute = Calendar.getInstance().get(Calendar.MINUTE);
-//
-//            mtimeEditText.setText(String.format("%02d:%02d", currentHour, currentMinute));
-//        }
-
         alertTimestamps = new ArrayList<>();
         malertsViewAdapter = new AlertsViewAdapter(alertTimestamps);
 
@@ -63,7 +58,44 @@ public class MedicationEditActivity extends AppCompatActivity implements AddAler
         malertRecyclerview.setLayoutManager(new LinearLayoutManager(this));
         malertRecyclerview.setAdapter(malertsViewAdapter);
 
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
+        onAlertSwipe().attachToRecyclerView(malertRecyclerview);
+    }
+
+    public void onAddAlert(View view) {
+        DialogFragment newFragment = new AddAlertDialogFragment();
+        newFragment.show(getSupportFragmentManager(), "addAlert");
+    }
+
+    @Override
+    public void onDialogPositiveClick(Integer hour, Integer minute, List<Integer> weekdays) {
+        alertTimestamps.add(new AlertTimestamp(hour, minute, weekdays));
+        malertsViewAdapter.notifyItemInserted(alertTimestamps.size());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.save_medication) {
+            // TODO: save medication.
+
+//            Medication medication = new Medication(name, description, timestamps);
+//            Intent intent = new Intent();
+//            intent.putExtra(MEDICATION_OBJECT, medication);
+//            setResult(RESULT_OK, intent);
+//            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.medication_menu, menu);
+        return true;
+    }
+
+    private ItemTouchHelper onAlertSwipe() {
+        return new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
                 return false;
@@ -78,32 +110,11 @@ public class MedicationEditActivity extends AppCompatActivity implements AddAler
                 malertsViewAdapter.notifyItemRemoved(position);
 
                 Snackbar.make(malertRecyclerview, R.string.alert_deletion_message, Snackbar.LENGTH_LONG)
-                .setAction("Undo", v -> {
-                    alertTimestamps.add(position, deletedTimestamp);
-                    malertsViewAdapter.notifyItemInserted(position);
-                }).show();
+                        .setAction("Undo", v -> {
+                            alertTimestamps.add(position, deletedTimestamp);
+                            malertsViewAdapter.notifyItemInserted(position);
+                        }).show();
             }
-        }).attachToRecyclerView(malertRecyclerview);
-    }
-
-    public void onAddAlert(View view) {
-        DialogFragment newFragment = new AddAlertDialogFragment();
-        newFragment.show(getSupportFragmentManager(), "addAlert");
-    }
-
-    @Override
-    public void onDialogPositiveClick(Integer hour, Integer minute, List<Integer> weekdays) {
-        alertTimestamps.add(new AlertTimestamp(hour, minute, weekdays));
-        malertsViewAdapter.notifyItemInserted(alertTimestamps.size());
-    }
-
-    public void onSaveMedication(View view) {
-//        Medication medication = new Medication(name, description, timestamps);
-
-//        Intent intent = new Intent();
-//        intent.putExtra(MEDICATION_OBJECT, medication);
-//
-//        setResult(RESULT_OK, intent);
-//        finish();
+        });
     }
 }
